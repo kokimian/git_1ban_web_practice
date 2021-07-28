37841f16af0cc10f821fe6a03b5d8469a1b1db66
SUNGMIN KIM <obingguo@naver.com>
2021-07-29 02:57:22 +0900

    2021/07/28 web practice 1

--- profileapp/models.py
+++ profileapp/models.py
@@ -3,10 +3,15 @@ from django.db import models
 
 # Create your models here.
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE,
-                                related_name='profile')
+# 0. pip install pillow
+# 1. profile - account 1:1 매칭 시키기
+# 2. migration 하기 python mange.py makemigrations
+# 3. python mange.py migrate
 
-    image = models.ImageField(upload_to='profile/', null=True)  # settings에서 설정한 MEDIA_ROOT의 media 폴더 아래에 profile 폴더에 저장
-    nickname = models.CharField(max_length=20, unique=True, null=True) #수업과 다름 null 값 추후 입력안하면 입력하라는 방식으로 만들 예정
-    message = models.CharField(max_length=100, null=True)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,  #CASCADE는 연결된 계정이 삭제되면 함께 삭제,
+                                related_name='profile')
+    image = models.ImageField(upload_to='profile/', null=True)
+    nickname = models.CharField(max_length=30, unique=True)
+    message = models.CharField(max_length=200, null=True)
--- profileapp/views.py
+++ profileapp/views.py
@@ -1,15 +1,40 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.urls import reverse_lazy, reverse
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView, UpdateView
 
+from profileapp.decorators import profile_ownership_required
 from profileapp.forms import ProfileCreationForm
-from profile import Profile
+from profileapp.models import Profile
 
 
+@method_decorator(profile_ownership_required, 'get')
+@method_decorator(profile_ownership_required, 'post')
 class ProfileCreateView(CreateView):
     model = Profile
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    #success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/create.html'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk})
+
+# accounts 로 설정 안한사람은 login_required(안에 주소)커스텀해야한다
+@method_decorator(profile_ownership_required, 'get')
+@method_decorator(profile_ownership_required, 'post')
+class ProfileUpdateView(UpdateView):
+    model = Profile
+    form_class = ProfileCreationForm
+    context_object_name = 'target_profile'
+    template_name = 'profileapp/update.html'
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk})
+#self.object가 target_user라고 생각해야함 profileapp의 유저가 아니라 accountapp의 유저로 가야하므로 .user필요
